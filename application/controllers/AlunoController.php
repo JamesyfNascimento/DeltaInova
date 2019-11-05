49917e9360660b87a304b65b3b00b512911aa721
James <jamesventosa@hotmail.com>
2019-11-05 18:13:20 -0300

    metodo adicionar aluno criado na controller do aluno

--- application/controllers/AlunoController.php
+++ application/controllers/AlunoController.php
@@ -1,22 +1,40 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
 
-class AlunosController extends REST_Controller {
+header('Access-Control-Allow-Origin: *');
+
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+	header('Access-Control-Allow-Headers: Content-Type');
+	exit;
+}
+
+class AlunoController extends REST_Controller {
 
 	public function __construct(){
-			parent::__construct();
-			$this->load->model('Alunos' , 'Alunos');
+            parent::__construct();
+			$this->load->model('Aluno' , 'Aluno');
 	}
 		
 	//retorna a lista dos alunos
-	public function getAlunos(){
-        $data = $this->Alunos->getAlunos();
+	public function index_get(){
+        $data = $this->Aluno->getAlunos();
         print_r($data);
 		if ($data) {
             $this->response($data, 200);
         } else {
             $this->response(array(), 200);
         }
-	}
+    }
+
+
+    public function index_post(){
+        $input = $this->input->post();
+        $this->Aluno->addAlunos($input);
+        $this->response(['Item created successfully.'], REST_Controller::HTTP_OK);
+    } 
+    
 }
