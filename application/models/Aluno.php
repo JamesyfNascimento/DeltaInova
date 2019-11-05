86d38ac20f957160e0736226d1eee63ec4e3d1c9
James <jamesventosa@hotmail.com>
2019-11-05 18:13:55 -0300

    Metodo para inserir aluno no banco criado na model

--- application/models/Aluno.php
+++ application/models/Aluno.php
@@ -1,11 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Alunos extends CI_Model{
+class Aluno extends CI_Model{
 
     // retorna  todos os alunos  do banco de dados
     public function getAlunos(){
         $query = $this->db->get('Alunos');
         return $query->result();
+    }
+
+    // persiste o aluno no banco
+    public function  addAlunos($data = NULL){
+        if($data != NULL){
+            $this->db->insert('Alunos', $data);
+        }
     }
 }
